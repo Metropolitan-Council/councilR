52df0ceb04c984cb009fa81101c59250c7426a99
Liz Roten <liz.roten@metc.state.mn.us>
2020-05-13 14:04:24 -0500

    update testing to test for usethis

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -2,15 +2,15 @@
 
 test_that("requires_pkg", {
   missing_pkg <- function() requires_pkg("flarf")
-  has_pkg <- function() requires_pkg("shiny")
+  has_pkg <- function() requires_pkg("usethis")
   missing_pkg_v <- function() requires_pkg_version("flarf", "1.2")
-  outdated_pkg <- function() requires_pkg_version("shiny", "98.0")
-  uptodate_pkg <- function() requires_pkg_version("shiny", "1.3.0")
+  outdated_pkg <- function() requires_pkg_version("usethis", "1.0.0")
+  uptodate_pkg <- function() requires_pkg_version("usethis", "1.6.0")
 
   expect_silent(has_pkg())
   expect_silent(uptodate_pkg())
 
   expect_error(missing_pkg(), "missing_pkg.+requires.+flarf")
   expect_error(missing_pkg_v(), "missing_pkg_v.+requires.+flarf")
-  expect_error(outdated_pkg(), "outdated_pkg.+requires.+shiny.+version.+98")
+  expect_null(outdated_pkg())
 })
