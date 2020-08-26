5e176def0e24b92b1b9a58e8fdc68ef4d0168c2a
Liz Roten <liz.roten@metc.state.mn.us>
2020-08-26 11:47:27 -0500

    add "dbname" parameter for different db names

--- R/import_from_gis.R
+++ R/import_from_gis.R
@@ -1,22 +1,24 @@
 #' Import a dataset from ArcCatalog
 #'
-#' @param query a string with the database connection and feature class
+#' @param query character, string with the database connection and feature class
+#' @param dbname character, database name. Usually either `"GIS"` or `"GISLibrary"`
 #'
-#' @return an \code{sf} object
+#' @return an `sf` object
 #' @export
 #' @examples \dontrun{import_from_gis("GISTransit.dbo.PublicParcelsMetroCTUs")}
 #' @importFrom sf st_as_sf
 #' @importFrom odbc odbc dbConnect
 #' @importFrom DBI dbGetQuery dbDisconnect
 #' @importFrom tictoc tic toc
-import_from_gis <- function(query){
+import_from_gis <- function(query,
+                            dbname = "GISLibrary"){
   tictoc::tic()
 
-  if(DBI::dbCanConnect(odbc::odbc(), "GIS") != TRUE){
+  if(DBI::dbCanConnect(odbc::odbc(), dbname) != TRUE){
     stop("Database could not connect.")
   }
 
-  conn <- odbc::dbConnect(odbc::odbc(), "GIS")
+  conn <- odbc::dbConnect(odbc::odbc(), dbname)
 
   sf_df <- sf::st_as_sf(DBI::dbGetQuery(conn,
                                         paste0("SELECT *, Shape.STAsText() as wkt FROM ", query)),
--- tests/testthat/test-import_from_gis.R
+++ tests/testthat/test-import_from_gis.R
@@ -1,7 +1,7 @@
 test_that("error message is returnd", {
 
   if(DBI::dbCanConnect(odbc::odbc(), "GIS") != TRUE){
-    testthat::expect_error(import_from_gis("GISTransit.dbo.PublicParcelsMetroCTUs"))
+    testthat::expect_error(import_from_gis("GISTransit.dbo.PublicParcelsMetroCTUs", dbname = "GIS"))
   }
 
   })
