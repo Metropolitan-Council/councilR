a9a418728bf23835ce1f99fa75e5103755dae838
Liz Roten <liz.roten@metc.state.mn.us>
2020-10-21 11:43:56 -0500

    test just on class (not ideal, need to change later)

--- tests/testthat/test-import_from_gpkg.R
+++ tests/testthat/test-import_from_gpkg.R
@@ -1,11 +1,7 @@
 testthat::skip_if_offline()
 
-if(osVersion != "windows"){
 
-testthat::expect_warning(import_from_gpkg("https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/plan_parks_regional/gpkg_plan_parks_regional.zip"))
-
-} else {
   testthat::expect_true(class(import_from_gpkg("https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/plan_parks_regional/gpkg_plan_parks_regional.zip"))[[1]] == "sf")
-}
+
 
 # testthat::expect_true(class(import_from_gpkg("https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/plan_parks_regional/gpkg_plan_parks_regional.zip"))[[1]] == "sf")
