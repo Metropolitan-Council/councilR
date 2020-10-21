5710c02befa72f91b3badaf47ad2ea1defba8d7d
Liz Roten <liz.roten@metc.state.mn.us>
2020-10-21 11:20:30 -0500

    re-write test

--- tests/testthat/test-import_from_gpkg.R
+++ tests/testthat/test-import_from_gpkg.R
@@ -1,8 +1,9 @@
 testthat::skip_if_offline()
 
-testthat::skip_on_os("windows"){
-testthat::expect_warning(import_from_gpkg("https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/plan_parks_regional/gpkg_plan_parks_regional.zip"))
-}
+if(osVersion != "windows"){
 
+testthat::expect_warning(import_from_gpkg("https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/plan_parks_regional/gpkg_plan_parks_regional.zip"))
+
+}
 
 # testthat::expect_true(class(import_from_gpkg("https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/plan_parks_regional/gpkg_plan_parks_regional.zip"))[[1]] == "sf")
