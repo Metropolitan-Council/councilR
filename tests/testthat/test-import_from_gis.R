f94bfd504505f288bbe0edf94b544468e9a579ac
Liz Roten <liz.roten@metc.state.mn.us>
2020-10-21 10:00:39 -0500

    specify using testthat

--- tests/testthat/test-import_from_gis.R
+++ tests/testthat/test-import_from_gis.R
@@ -1,7 +1,8 @@
-test_that("error message is returnd", {
+testthat::test_that("error message is returnd", {
 
   if(DBI::dbCanConnect(odbc::odbc(), "GIS") != TRUE){
-    testthat::expect_error(import_from_gis("GISTransit.dbo.PublicParcelsMetroCTUs", dbname = "GIS"))
+    testthat::expect_error(import_from_gis("GISTransit.dbo.PublicParcelsMetroCTUs",
+                                           dbname = "GIS"))
   }
 
   })
