af54453e561031da073c25cf34a7fe09a260ad3b
Liz Roten <liz.roten@metc.state.mn.us>
2022-03-09 12:06:30 -0600

    add system requirements

--- R/import_from_gis.R
+++ R/import_from_gis.R
@@ -7,6 +7,9 @@
 #' @param pwd character, user password. Default is `getOption("councilR.pwd")`.
 #'
 #' @note See `vignette("Options")` to review package options.
+#'     You must be set up with the appropriate database drivers to use this function.
+#'     **Windows** users need ODBC with Microsoft SQL. Contact IS support for ODBC installation.
+#'     **Mac** users need `unixodbc` and `freetds`. See instructions in [`{MetroTransitR}`](https://github.com/Metropolitan-Council/MetroTransitR)
 #' @return an `sf` object
 #' @export
 #' @examples \dontrun{
