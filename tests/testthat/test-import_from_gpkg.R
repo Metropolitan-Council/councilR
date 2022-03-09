05f87a9b809f6004c3f3671dfaf13d8013765c9d
Liz Roten <liz.roten@metc.state.mn.us>
2022-03-09 10:28:38 -0600

    update test to pull council districts
    smaller than regional parks data, no warnings generated from readOGR

--- tests/testthat/test-import_from_gpkg.R
+++ tests/testthat/test-import_from_gpkg.R
@@ -1,7 +1,7 @@
 testthat::skip_if_offline()
 
 
-testthat::expect_true(class(import_from_gpkg("https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/plan_parks_regional/gpkg_plan_parks_regional.zip"))[[1]] == "sf")
+testthat::expect_true(class(import_from_gpkg("https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/bdry_mcdistricts_2013/gpkg_bdry_mcdistricts_2013.zip"))[[1]] == "sf")
 
 
 # testthat::expect_true(class(import_from_gpkg("https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/plan_parks_regional/gpkg_plan_parks_regional.zip"))[[1]] == "sf")
