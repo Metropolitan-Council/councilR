57db070f7db7c3b14cbeaa4da6b58bc908a80d5f
Liz Roten <liz.roten@metc.state.mn.us>
2023-08-04 13:19:57 -0500

    add test - currently failing on Mac

---
+++ tests/testthat/test-import_from_FRED.R
@@ -0,0 +1,29 @@
+# Can import_from_gis() actually import from the GISLibrary database?
+
+# skip on GH Actions
+testthat::skip_on_ci()
+
+# skip if not connected to VPN
+testthat::skip_if(
+  is.null(curl::nslookup("mc.local"))
+)
+
+testthat::skip_if(httr2::secret_has_key("QUHBRb_yoy2RRj59qno8NVXA7mW402xkins"))
+
+
+testthat::test_that("group quarters dataset", {
+
+  gq <- import_from_FRED(
+    table_name = "GQ_UNIT",
+    prod = FALSE,
+    uid = httr2::secret_decrypt("QUHBRb_yoy2RRj59qno8NVXA7mW402xkins", "COUNCILR_KEY"),
+    pwd = httr2::secret_decrypt("rQHk4S39pjfJ6yoKWUUNpQUDk2i9XA3d", "COUNCILR_KEY")
+  )
+
+  # test that all airports are included
+  # there should be 14 airports
+  testthat::expect_equal(nrow(gq), 14)
+
+  # test that object returned is an sf object
+  testthat::expect_equal(class(gq)[[1]], "dataframe")
+})
