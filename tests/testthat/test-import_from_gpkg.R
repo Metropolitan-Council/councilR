015d6acb48c49710e3e569e123761e202bbf4828
Liz Roten <liz.roten@metc.state.mn.us>
2024-11-20 14:57:58 -0600

    update test to check for file name bug

--- tests/testthat/test-import_from_gpkg.R
+++ tests/testthat/test-import_from_gpkg.R
@@ -1,14 +1,5 @@
 testthat::skip_if_offline()
 
-testthat::test_that("metc districts with import from", {
-  districts <- import_from_gpkg("https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/bdry_mcdistricts_2013/gpkg_bdry_mcdistricts_2013.zip")
-
-  testthat::expect_s3_class(districts, "sf")
-
-  testthat::expect_equal(nrow(districts), 16)
-})
-
-
 
 testthat::test_that("metc districts with import_gpkg", {
   districts <- import_gpkg("https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/bdry_mcdistricts_2013/gpkg_bdry_mcdistricts_2013.zip")
@@ -28,4 +19,15 @@ testthat::test_that("EJ TPP layer",  {
 
   testthat::expect_equal(nrow(ej_layer), 797)
 })
+
+
+testthat::test_that("AADT layer",  {
+  aadt <- import_gpkg("https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_dot/trans_aadt_traffic_count_locs/gpkg_trans_aadt_traffic_count_locs.zip") %>%
+    suppressWarnings()
+
+  testthat::expect_s3_class(aadt, "sf")
+
+  testthat::expect_gte(nrow(aadt), 39728)
+})
+
 
