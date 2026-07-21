97cd17db184deb4d554801a976c26b007edde44d
Liz Roten <liz.roten@metc.state.mn.us>
2026-07-21 15:39:53 -0500

    deprecate import_from_gpkg

--- R/import_from_gpkg.R
+++ R/import_from_gpkg.R
@@ -1,4 +1,4 @@
-#' @title Import an `sf` object using a geopackage URL
+#' @title DEPRECATED Import an `sf` object using a geopackage URL
 #'
 #' @param link character, URL for the `.gpkg` object
 #' @param save_file logical, whether to save the downloaded file.
@@ -15,7 +15,8 @@
 #' @return [sf::sf()] object
 #' @export
 #'
-#' @description This function is particularly useful for importing data from
+#' @description Deprecated.
+#'     This function is particularly useful for importing data from
 #'     [Minnesota Geospatial Commons](https://gisdata.mn.gov/)
 #'     when access to GISLibrary is unavailable.
 #'
@@ -25,6 +26,7 @@
 #' @importFrom utils download.file tail unzip
 #' @importFrom data.table last
 #' @importFrom purrr map
+#' @importFrom cli cli_warn
 #' @examples
 #'
 #' library(councilR)
@@ -41,6 +43,14 @@ import_from_gpkg <- function(link,
                              keep_temp = FALSE,
                              .quiet = TRUE,
                              ...) {
+  cli::cli_warn(
+    message = c(
+      "import_from_gpkg() is no longer supported.",
+      "i" = "Please modify your code to use {.href [arcgislayers](https://r.esri.com/arcgislayers/)} instead.",
+      "i" = "See {.vignette councilR::Spatial_helpers} for more information and examples."
+    ),
+    wrap = TRUE
+  )
   # check input types
   purrr::map(
     c(link),
